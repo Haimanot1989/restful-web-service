fd1fa52603912aac9737cff3e255025a39ef0740
Tekie <haitekie@local.finn.no>
2019-05-03 17:04:05 +0200

    Step 04 - Enhancing the Hello World Service to return a Bean

---
+++ src/main/java/com/itsisterhood/rest/webservices/restfulwebservices/HelloWorldBean.java
@@ -0,0 +1,17 @@
+package com.itsisterhood.rest.webservices.restfulwebservices;
+
+public class HelloWorldBean {
+    private String message;
+
+    public HelloWorldBean(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
--- src/main/java/com/itsisterhood/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/itsisterhood/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -10,4 +10,9 @@ public class HelloWorldController {
         return "Hello World";
     }
 
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean(){
+        return new HelloWorldBean("Hello World");
+    }
 }
