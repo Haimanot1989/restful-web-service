0a4e5cd5a9e3c18d2c22c7a573fa54481f26f003
Tekie <haitekie@local.finn.no>
2019-05-03 21:54:58 +0200

    Step 06 - Enchancing the Hello World Service with a Path Variable

--- src/main/java/com/itsisterhood/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/itsisterhood/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.itsisterhood.rest.webservices.restfulwebservices;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +15,10 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world-bean")
     public HelloWorldBean helloWorldBean(){
         return new HelloWorldBean("Hello World");
+    }
+
+    @GetMapping(path = "/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name){
+        return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 }
