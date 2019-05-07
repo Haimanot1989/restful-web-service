5656e30801dbb0e4cb40b9297b7467992c9beec0
haimanot1989 <haimif@yahoo.com>
2019-05-07 08:18:31 +0200

    Step 26 - Versioning RESTful Services - Basic Approach with URIs

---
+++ src/main/java/com/itsisterhood/rest/webservices/restfulwebservices/versioning/PersonV1.java
@@ -0,0 +1,22 @@
+package com.itsisterhood.rest.webservices.restfulwebservices.versioning;
+
+public class PersonV1 {
+    private String name;
+
+    public PersonV1() {
+    }
+
+    public PersonV1(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
